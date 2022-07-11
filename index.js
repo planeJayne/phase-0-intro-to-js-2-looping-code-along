78d6f036ce7d4b4f1dc7a3b1a457b9d3a18b86dc
Jasmin <jasmin.parish@outlook.com>
2022-07-11 17:37:23 -0400

    commit tree 62a93bbeb09e56120fe06bccfcb6c059a4abd2fd

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+  /* const gifts = ["teddy bear", "drone", "doll"];
+   function wrapGifts(gifts) {
+    for (let i=0; i< gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+    return gifts;
+   }
+   wrapGifts(gifts);*/
+
+   const names =["Guadalupe", "Ollie", "Aki"];
+   let messages =[];
+
+   function writeCards(names,event) {
+        for( let i =0 ; i < names.length; i++){
+            messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        }
+       
+    return messages
+   }
+
+function countDown(int) {
+    let i =10
+    while(i >= 0) {
+        console.log(i)
+        i --
+    }
+}
